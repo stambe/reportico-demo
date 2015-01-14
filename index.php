7b8e5408dd8af27d827026600f25a99a65b976a4
Sagar Tambe <sagar@Sagars-Mac-mini-2.local>
2015-01-14 19:22:40 +0530

    added index and reportico

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Reportico Demo</title>
+<style type="text/css">
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .9em;
+        color: #000000;
+        background-color: #FFFFFF;
+        margin: 0;
+        padding: 10px 20px 20px 20px;
+    }
+</style>
+</head>
+
+<body>
+    <p><img src="../MAMP-PRO-Logo.png" id="logo" alt="MAMP PRO Logo" width="250" height="49" /></p>
+    <?php
+        print "asdf";
+    ?>
+</body>
+</html>
